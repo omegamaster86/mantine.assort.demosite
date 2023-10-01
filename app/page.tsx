eefa28b8e7902bcebaad6a8562087c97cee2393a
omegamaster86 <masya868686@gmail.com>
2023-10-01 15:57:19 +0900

    add : title component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import {Title} from './components/title/title'
 
 export default function Home() {
   return (
     <main>
-      <div></div>
+      <Title/>
     </main>
   )
 }
