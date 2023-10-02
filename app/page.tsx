47f2167950bb817a6d87684524111fdf3c22d798
omegamaster86 <masya868686@gmail.com>
2023-10-02 23:30:08 +0900

    add : header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import {Header} from './components/header/header'
 import {Title} from './components/title/title'
 
 export default function Home() {
   return (
     <main>
+      <Header/>
       <Title/>
     </main>
   )
