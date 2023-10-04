80fca00db4f66d2b2eddc7551eb40ed6a8467ed2
omegamaster86 <masya868686@gmail.com>
2023-10-04 17:34:03 +0900

    add : color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'regal-red': '#f03e3e',
+        'regal-purple': '#ae3ec9',
+      },
     },
     fontFamily: {
       'greycliff': ['Greycliff','CF'],
