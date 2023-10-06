ce9bb5fff0c54a0147aa7a81cf0537fea86d11fb
omegamaster86 <masya868686@gmail.com>
2023-10-06 19:24:34 +0900

    repair : button color change

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,16 +1,13 @@
 'use client';
-
 import { useState } from 'react';
-import { Container, Group, Burger } from '@mantine/core';
+import { Container, Group, Burger, Button } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { MantineLogo } from '@mantine/ds';
-import classes from './header.module.css';
+
 
 const links = [
   { link: '/about', label: 'Features' },
-  { link: '/pricing', label: 'Pricing' },
-  { link: '/learn', label: 'Learn' },
-  { link: '/community', label: 'Community' },
+  { link: '/', label: 'ダッシュボード' },
 ];
 
 export const Header = () => {
@@ -18,28 +15,30 @@ export const Header = () => {
   const [active, setActive] = useState(links[0].link);
 
   const items = links.map((link) => (
-    <a
+    <Button
       key={link.label}
-      href={link.link}
-      className={classes.link}
+      component="a"
+      href="http://localhost:3000/"
+      className='block leading-none py-2 px-3 no-underline font-extrabold rounded text-white'
       data-active={active === link.link || undefined}
+      variant="gradient"
+      gradient={{ from: 'red', to: 'grape' }}
       onClick={(event) => {
         event.preventDefault();
         setActive(link.link);
       }}
     >
       {link.label}
-    </a>
+    </Button>
   ));
 
   return (
     <header className='h-14 pt-7'>
-      <Container size="md" className={classes.inner}>
+      <Container size="md" className='h-14 flex justify-between items-center'>
         <MantineLogo size={28} />
-        <Group gap={5} visibleFrom="xs">
-          {items}
-        </Group>
-
+            <Group gap={5} visibleFrom="xs">
+                  {items}
+            </Group>
         <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
       </Container>
     </header>
