07efb106aec22a1a09283638c8700a8c3bb30cff
omegamaster86 <masya868686@gmail.com>
2023-10-07 11:29:38 +0900

    repair : mantine button

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,43 +1,43 @@
 'use client';
-import { useState } from 'react';
 import { Container, Group, Burger, Button } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { MantineLogo } from '@mantine/ds';
 
-
-const links = [
-  { link: '/about', label: 'Features' },
-  { link: '/', label: 'ダッシュボード' },
-];
+const Menu = () => {
+    return(
+        <div>
+            <Button
+            component="a"
+            href="https://github.com/mantinedev/mantine"
+            size="md"
+            className='h-14 px-9 mr-5'
+            color="black"
+            >
+            Slackに追加
+            </Button>
+            <Button
+            component="a"
+            href="https://github.com/mantinedev/mantine"
+            size="md"
+            variant="gradient"
+            gradient={{ from: 'red', to: 'grape' }}
+            className='h-14 px-9'
+            >
+            ダッシュボード
+            </Button>
+        </div>
+        );
+}
 
 export const Header = () => {
   const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
-
-  const items = links.map((link) => (
-    <Button
-      key={link.label}
-      component="a"
-      href="http://localhost:3000/"
-      className='block leading-none py-2 px-3 no-underline font-extrabold rounded text-white'
-      data-active={active === link.link || undefined}
-      variant="gradient"
-      gradient={{ from: 'red', to: 'grape' }}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-      }}
-    >
-      {link.label}
-    </Button>
-  ));
 
   return (
     <header className='h-14 pt-7'>
       <Container size="md" className='h-14 flex justify-between items-center'>
         <MantineLogo size={28} />
             <Group gap={5} visibleFrom="xs">
-                  {items}
+              <Menu/>
             </Group>
         <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
       </Container>
