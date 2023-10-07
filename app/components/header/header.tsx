be3b737fbc73242dc8ca3befbc37bbc0047ac593
omegamaster86 <masya868686@gmail.com>
2023-10-07 13:54:45 +0900

    add : Slack icon

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -2,6 +2,7 @@
 import { Container, Group, Burger, Button } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { MantineLogo } from '@mantine/ds';
+import Image from "next/image";
 
 const Menu = () => {
     return(
@@ -12,7 +13,9 @@ const Menu = () => {
             size="md"
             className='h-14 px-9 mr-5'
             color="black"
+            // leftSection={<BrandSlack size={14} />}
             >
+            <Image src="/assets/Slackicon.png" width={16} height={16} alt='logo' className=' mr-3'/>
             Slackに追加
             </Button>
             <Button
