54fb828fdf4c7afbe58cf1e121195eb4a137ece5
omegamaster86 <masya868686@gmail.com>
2023-10-08 20:48:23 +0900

    add : className

--- app/components/body/sec1/section1.tsx
+++ app/components/body/sec1/section1.tsx
@@ -36,13 +36,13 @@ import {
   export const FeaturesCards1 = () => {
     const theme = useMantineTheme();
     const features = mockdata.map((feature) => (
-      <Card key={feature.title} shadow="md" radius="md" className={classes.card} padding="xl">
+      <Card key={feature.title} shadow="md" radius="md" className="" padding="xl">
         <feature.icon
           style={{ width: rem(50), height: rem(50) }}
           stroke={2}
           color={theme.colors.blue[6]}
         />
-        <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+        <Text fz="lg" fw={500} className="" mt="md">
           {feature.title}
         </Text>
         <Text fz="sm" c="dimmed" mt="sm">
@@ -59,11 +59,11 @@ import {
           </Badge>
         </Group>
   
-        <Title order={2} className={classes.title} ta="center" mt="sm">
+        <Title order={2} className="" ta="center" mt="sm">
           Integrate effortlessly with any technology stack
         </Title>
   
-        <Text c="dimmed" className={classes.description} ta="center" mt="md">
+        <Text c="dimmed" className="" ta="center" mt="md">
           Every once in a while, you’ll see a Golbat that’s missing some fangs. This happens when
           hunger drives it to try biting a Steel-type Pokémon.
         </Text>
