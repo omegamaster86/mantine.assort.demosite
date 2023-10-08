4136ae6809bd11f19c2769e6372629b792804028
omegamaster86 <masya868686@gmail.com>
2023-10-08 19:44:13 +0900

    add : Attention

--- app/components/title/title.tsx
+++ app/components/title/title.tsx
@@ -1,46 +1,74 @@
-import { Container, Text, Button, Group } from '@mantine/core';
-import { GithubIcon } from '@mantine/ds';
-import classes from './title.module.css';
+
+import { Container, Text, Button } from '@mantine/core';
 import Balancer from 'react-wrap-balancer'
+import Image from "next/image";
+import { ExclamationMark } from 'tabler-icons-react';
+
+export const Menu = () => {
+ return (
+    <div>
+          <Button
+          component="a"
+          href="https://github.com/mantinedev/mantine"
+          size="xl"
+          className='h-15 px-9 mr-5'
+          color="black"
+          >
+          <Image src="/assets/Slackicon.png" width={15} height={15} alt='logo' className=' mr-3'/>
+          Slackに追加
+          </Button>
+          <Button
+          component="a"
+          href="https://github.com/mantinedev/mantine"
+          size="xl"
+          variant="gradient"
+          gradient={{ from: 'red', to: 'grape' }}
+          className='h-14 px-9'
+          >
+          ダッシュボード
+          </Button>
+    </div>
+ )
+}
+
+export const Attention = () => {
+  return(
+  <div>
+    <div className=' py-3 mt-28 border-solid border-red-600 border'>
+      <div className='flex font-bold justify-center items-center'>
+            <ExclamationMark
+              size={18}
+              strokeWidth={2}
+              color={'red'}
+              className='border-solid rounded-full border-red-600'
+            />
+         <div className=' text-red-500 pl-3.5'><Balancer>既にAssortをお使いの方は、アプリをインストールし直す必要があります</Balancer></div>
+      </div>
+      <div className=''><Balancer>アップデートによりアプリの再インストールが必要になりました。お手数ですが、</Balancer></div>{' '}
+      <div><Balancer>「Slackに追加」ボタンから再度インストールをお願いいたします。</Balancer></div>
+    </div>
+  </div>
+  )
+}
 
 export const Title= () => {
   return (
-    <div className='relative box-border text-center m-0'>
-      <Container  className='relative  my-32 mx-0 sm:my-20'>
-        <h1 className='text-4xl font-black p-0 m-0 md:text-6xl'>
-          <Text component="span" variant="gradient" gradient={{ from: 'red', to: 'grape' }} inherit>
+    <div className='relative box-border my-32 max-w-full mx-4 text-center m-0 sm:my-20'>
+      <Container>
+        <h1 className='text-5xl font-black p-0 max-w-full inline-block md:text-6xl lg:text-7xl'>
+          <Text variant="gradient" gradient={{ from: 'red', to: 'grape' }} inherit>
               <Balancer>All-in-One App for Slack</Balancer>
           </Text>
         </h1>
-
-        <Text className={classes.description} color="dimmed">
-            <Balancer>
-                Build fully functional accessible web applications with ease – Mantine includes more than
-                100 customizable components and hooks to cover you in any situation
-            </Balancer>
-        </Text>
-
-        <Group className={classes.controls}>
-          <Button
-            size="xl"
-            className={classes.control}
-            variant="gradient"
-            gradient={{ from: 'blue', to: 'cyan' }}
-          >
-            Get started
-          </Button>
-
-          <Button
-            component="a"
-            href="https://github.com/mantinedev/mantine"
-            size="xl"
-            variant="default"
-            className={classes.control}
-            leftSection={<GithubIcon size={20} />}
-          >
-            GitHub
-          </Button>
-        </Group>
+        <div className="inline-block max-w-screen-md text-2xl">
+            <Balancer>オールインワンのSlackアプリ Assort で生産性を向上しよう。Assort には業務効率化に役立つたくさんの機能が備わっています。</Balancer>
+        </div>
+        <div className='mt-12'>
+          <Menu/>
+        </div>
+        <div className='w-4/5 mx-auto'>
+          <Attention/>
+        </div>
       </Container>
     </div>
   );
