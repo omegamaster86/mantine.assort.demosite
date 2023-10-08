33992248ad1c29a40dc0a91c31f3b9b845181704
omegamaster86 <masya868686@gmail.com>
2023-10-08 12:07:24 +0900

    remove : group

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { Container, Group, Burger, Button } from '@mantine/core';
+import { Container, Burger, Button } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { MantineLogo } from '@mantine/ds';
 import Image from "next/image";
 
-const Menu = () => {
+export const Menu = () => {
     return(
         <div>
             <Button
@@ -13,7 +13,6 @@ const Menu = () => {
             size="md"
             className='h-15 px-9 mr-5'
             color="black"
-            // leftSection={<BrandSlack size={14} />}
             >
             <Image src="/assets/Slackicon.png" width={15} height={15} alt='logo' className=' mr-3'/>
             Slackに追加
@@ -39,9 +38,7 @@ export const Header = () => {
     <header className='h-14 pt-7'>
       <Container size="md" className='h-14 flex justify-between items-center'>
         <MantineLogo size={28} />
-            <Group gap={5} visibleFrom="xs">
-              <Menu/>
-            </Group>
+          <Menu/>
         <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
       </Container>
     </header>
