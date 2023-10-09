9f5e9e180acb4970f6173252dbd67513fc71311c
omegamaster86 <masya868686@gmail.com>
2023-10-09 16:12:05 +0900

    add : FeaturesCards1

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import {Header} from './components/header/header'
 import {Title} from './components/title/title'
+import {FeaturesCards1} from './components/body/sec1/section1'
 
 export default function Home() {
   return (
     <main>
       <Header/>
       <Title/>
+      <FeaturesCards1/>
     </main>
   )
 }
