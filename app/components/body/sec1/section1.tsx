0ab0ad812dc2f7e83a0d8dcebc5138e9f59f2809
omegamaster86 <masya868686@gmail.com>
2023-10-10 22:33:21 +0900

    add : video component

--- app/components/body/sec1/section1.tsx
+++ app/components/body/sec1/section1.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
     Badge,
     Group,
@@ -8,41 +9,38 @@ import {
     Container,
     rem,
     useMantineTheme,
+    ThemeIcon,
+    AspectRatio,
   } from '@mantine/core';
-  import { IconGauge, IconUser, IconCookie } from '@tabler/icons-react';
-  
+  import { IconGauge, IconUser, IconNumber1 } from '@tabler/icons-react';
+  import Balancer from 'react-wrap-balancer'
+  import { Video1 } from '../../youtube/video1';
   
   const mockdata = [
     {
-      title: 'Extreme performance',
+      title: '設定レス・運用レス',
       description:
-        'This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit',
+        'Slackアプリをインストールするだけで使えます。クラウドベースのため運用は一切要りません。',
       icon: IconGauge,
     },
     {
-      title: 'Privacy focused',
+      title: 'プライバシーに配慮',
       description:
-        'People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma',
+        '私達はお客様の対話データを一切保持しません。データ漏洩を心配する必要はありません。',
       icon: IconUser,
-    },
-    {
-      title: 'No third parties',
-      description:
-        'They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves',
-      icon: IconCookie,
     },
   ];
   
   export const FeaturesCards1 = () => {
     const theme = useMantineTheme();
     const features = mockdata.map((feature) => (
-      <Card key={feature.title} shadow="md" radius="md" className="" padding="xl">
+      <Card key={feature.title} shadow="md" radius="md" className="border-solid" padding="xl">
         <feature.icon
           style={{ width: rem(50), height: rem(50) }}
           stroke={2}
           color={theme.colors.blue[6]}
         />
-        <Text fz="lg" fw={500} className="" mt="md">
+        <Text fz="lg" fw={500} className="after:content-[''] after:block after:bg-blue-600 after:w-11 after:h-0.5" mt="md">
           {feature.title}
         </Text>
         <Text fz="sm" c="dimmed" mt="sm">
@@ -52,23 +50,48 @@ import {
     ));
   
     return (
-      <Container size="lg" py="xl">
+      <Container fluid py={60} bg={'#fff5f5'}>
         <Group justify="center">
-          <Badge variant="filled" size="lg">
-            Best company ever
+            <ThemeIcon
+                variant="gradient"
+                size="lg"
+                aria-label="Gradient action icon"
+                gradient={{ from: 'red', to: 'grape' }}
+                radius="xl"
+                >
+                <IconNumber1/>
+            </ThemeIcon>
+        </Group>
+        <Group justify="center" >
+            <Text fw={800} fz={35} variant="gradient" gradient={{ from: 'red', to: 'grape' }} >
+                @assort
+            </Text>
+        </Group>
+  
+        <Title order={1} fw={900} fz={50} ta="center" mt="sm">
+            SlackでChatGPTの力を最大限に
+        </Title>
+  
+        <Text fz={20} ta="center" mt="md" >
+            <Balancer>チーム全員で使えるChatGPTを考えたことはありますか？</Balancer>{' '}
+            <Balancer>AIのチカラを最大限活用し、競合他社と差をつけましょう。</Balancer>
+        </Text>
+
+        <AspectRatio ratio={16 / 9} mt="4rem" w="100%">
+            <Video1/>
+        </AspectRatio>
+
+        <Group justify="center" pt={"5rem"}>
+          <Badge variant="gradient" gradient={{ from: 'red', to: 'grape' }} size="xl" >
+                FEATURES
           </Badge>
         </Group>
   
-        <Title order={2} className="" ta="center" mt="sm">
-          Integrate effortlessly with any technology stack
+        <Title order={1} fw={900} fz={50} ta="center" mt="sm" >
+            AssortでChatGPTを使う理由
         </Title>
   
-        <Text c="dimmed" className="" ta="center" mt="md">
-          Every once in a while, you’ll see a Golbat that’s missing some fangs. This happens when
-          hunger drives it to try biting a Steel-type Pokémon.
-        </Text>
-  
-        <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl" mt={50}>
+        <SimpleGrid cols={{ base: 1, md: 2 }} spacing="xl" mt={50}>
           {features}
         </SimpleGrid>
       </Container>
