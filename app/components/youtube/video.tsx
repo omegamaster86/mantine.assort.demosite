6672e7aed8e8dbfed5039e8d965f87a129166704
omegamaster86 <masya868686@gmail.com>
2023-10-11 21:45:33 +0900

    add : video1

---
+++ app/components/youtube/video.tsx
@@ -0,0 +1,12 @@
+
+export const Video1 = () => {
+    
+    return (
+        <iframe
+          src="https://www.youtube-nocookie.com/embed/LeYl91bEz1g"
+          title="Youtube Video Player"
+          allow="accelerometer; autoplay; clipboard-write; fullscreen; encrypted-media; gyroscope;"
+          
+        />
+    );
+}
