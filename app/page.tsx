9d5f46b569c6c89c37063cc4c9bb3096cf702449
omegamaster86 <masya868686@gmail.com>
2023-10-14 19:01:07 +0900

    add : FeaturesCards3

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import {Header} from './components/header/header'
 import {Title} from './components/title/title'
 import {FeaturesCards1} from './components/body/sec1/section1'
 import {FeaturesAsymmetrical} from './components/body/sec2/section2'
+import {FeaturesCards3} from './components/body/sec3/section3'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Title/>
       <FeaturesCards1/>
       <FeaturesAsymmetrical/>
+      <FeaturesCards3/>
     </main>
   )
 }
