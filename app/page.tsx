31579a67b9e2f6c127d89f543c44f91c4920298e
omegamaster86 <masya868686@gmail.com>
2023-10-14 18:08:39 +0900

    add : FeaturesAsymmetrical

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import {Header} from './components/header/header'
 import {Title} from './components/title/title'
 import {FeaturesCards1} from './components/body/sec1/section1'
+import {FeaturesAsymmetrical} from './components/body/sec2/section2'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header/>
       <Title/>
       <FeaturesCards1/>
+      <FeaturesAsymmetrical/>
     </main>
   )
 }
