56a628c502a5fd7406119a28757af9fcbc3383bb
omegamaster86 <masya868686@gmail.com>
2023-10-15 14:33:21 +0900

    delete : image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import {Header} from './components/header/header'
 import {Title} from './components/title/title'
 import {FeaturesCards1} from './components/body/sec1/section1'
