f221ea2d4bbcaf964eb3c0dbae8859c44a790b31
omegamaster86 <masya868686@gmail.com>
2023-10-15 13:17:48 +0900

    add : section4

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import {Title} from './components/title/title'
 import {FeaturesCards1} from './components/body/sec1/section1'
 import {FeaturesAsymmetrical} from './components/body/sec2/section2'
 import {FeaturesCards3} from './components/body/sec3/section3'
+import {Section4} from './components/body/sec4/section4'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <FeaturesCards1/>
       <FeaturesAsymmetrical/>
       <FeaturesCards3/>
+      <Section4/>
     </main>
   )
 }
